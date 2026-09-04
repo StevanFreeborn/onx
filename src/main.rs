791e3a6c6d86e1759d9efd49fe98f7f31731ff00
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-09-04 16:59:47 -0500

    feat: add main cli method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::process;
+
+use onx::{parse_cli_from, render_cli_error, run};
+
+#[tokio::main]
+async fn main() {
+  let cli = match parse_cli_from(std::env::args_os()) {
+    Ok(cli) => cli,
+    Err(err) => {
+      if err.code == 0 {
+        print!("{}", err.message);
+        process::exit(0);
+      }
+      eprintln!("{}", render_cli_error(&err, false));
+      process::exit(err.code)
+    }
+  };
+
+  if let Err(err) = run(cli).await {
+    eprintln!("{}", render_cli_error(&err, false));
+    process::exit(err.code)
+  }
 }
